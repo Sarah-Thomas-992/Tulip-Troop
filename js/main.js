614e5708ddce107bd866dc4cacfe34b45c71d41b
Sarah Thomas <129238460+Sarah-Thomas-992@users.noreply.github.com>
2023-05-11 23:50:02 -0700

    Update main.js with flower code

---
+++ js/main.js
@@ -0,0 +1,5 @@
+The Dragonfruit 
+<i class="fa-duotone fa-flower-tulip" style="--fa-primary-color: #fa00be; --fa-secondary-color: #d9ec37; --fa-secondary-opacity: 1;"></i>
+
+The Ghost 
+<i class="fa-duotone fa-flower-tulip" style="--fa-primary-color: #00c4c4; --fa-secondary-color: #ebebeb; --fa-secondary-opacity: 1;"></i>
