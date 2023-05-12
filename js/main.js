43260ff946dc60c5a7ccab4d3d88de107dfbccaa
Sarah Thomas <129238460+Sarah-Thomas-992@users.noreply.github.com>
2023-05-12 15:26:56 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-The Dragonfruit 
+Dark Pink  
 <i class="fa-duotone fa-flower-tulip" style="--fa-primary-color: #fa00be; --fa-secondary-color: #d9ec37; --fa-secondary-opacity: 1;"></i>
 
-The Ghost 
-<i class="fa-duotone fa-flower-tulip" style="--fa-primary-color: #00c4c4; --fa-secondary-color: #ebebeb; --fa-secondary-opacity: 1;"></i>
+Light Pink 
+
+Light Yellow 
+
+Gold 
+
+Lavender 
+
+Dark Purple 
+
+Red 
+
+Orange 
